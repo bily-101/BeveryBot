1552b458f3042a51ee04a58aee55460b8cbe0dea
bily101 <lawingvasiliy06@gmail.com>
2020-11-22 11:40:32 -0600

    Setting Up

--- main/src/main/java/Bot.java
+++ main/src/main/java/Bot.java
@@ -3,6 +3,6 @@ import net.dv8tion.jda.core.JDABuilder;
 
 public class Bot {
     public static void main(String[] args) throws Exception {
-        JDA jda = new JDABuilder("SAMPLE_TOKEN").build();
+        JDA jda = new JDABuilder("SAMPLE_TOKENd").build();
     }
 }
