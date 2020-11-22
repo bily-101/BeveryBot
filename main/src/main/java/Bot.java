5385d7c51fbb529310a403f8218eaa9939729ed7
bily101 <lawingvasiliy06@gmail.com>
2020-11-22 11:35:40 -0600

    Setting Up

---
+++ main/src/main/java/Bot.java
@@ -0,0 +1,8 @@
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+
+public class Bot {
+    public static void main(String[] args) throws Exception {
+        JDA jda = new JDABuilder("NzYwODg5NjI3Nzk1MTI4MzUy.X3Snrw.fyIUC5uUNgZUQrnzM4Ak_aMdnBg").build();
+    }
+}
