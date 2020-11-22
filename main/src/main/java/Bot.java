4a2e7b563b50ebc2f69a9a9231f9b798508a543e
bily101 <lawingvasiliy06@gmail.com>
2020-11-22 11:40:21 -0600

    Setting Up

--- main/src/main/java/Bot.java
+++ main/src/main/java/Bot.java
@@ -3,7 +3,6 @@ import net.dv8tion.jda.core.JDABuilder;
 
 public class Bot {
     public static void main(String[] args) throws Exception {
-        JDA jda = new JDABuilder("NzYwODg5NjI3Nzk1MTI4MzUy.X3Snrw.fyIUC5uUNgZUQrnzM4Ak_aMdnBg").build();
+        JDA jda = new JDABuilder("SAMPLE_TOKEN").build();
     }
 }
-//push
---
+++ main/src/main/java/events/helloEvent.java
@@ -0,0 +1,10 @@
+package events;
+
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class helloEvent extends ListenerAdapter {
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+
+    }
+}
